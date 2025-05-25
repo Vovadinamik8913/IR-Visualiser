da3efe0d3c5583f702b85e889b4c62de63bbe4c8
Vovik <v.abramenko@g.nsu.ru>
2025-05-25 23:28:10 +0700

    adding .env

--- Server/run.sh
+++ Server/run.sh
@@ -1,6 +1,6 @@
-export OPT_PATH="opt"
-export WORK_PATH="/home/abrikosjr/Desktop/pak/work"
-export DATASOURCE_URL="jdbc:sqlite:/home/abrikosjr/Desktop/pak/IR-Visualiser/Server/data/visualiser.db"
+export $(grep -v '^#' .env | xargs)
+mkdir data
+export DATASOURCE_URL="jdbc:sqlite:./data/visualiser.db"
 export FRONT_PATH="http://localhost:3000"
 ./gradlew -x test build
 ./gradlew bootRun
